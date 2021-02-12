e1dca167d6f84a76f182c4056878c00266e92792
Juan Vidal <juancarlosvidalp@hotmail.com>
2021-02-12 13:05:28 -0500

    JQuery

--- js/llenatubaul.js
+++ js/llenatubaul.js
@@ -10,10 +10,10 @@ class Productos {
 
 let baseDeDatos = [];
 
-let hersheyBlanca = new Productos("Chocolatina Hershey's Blanca",
+let hersheyBlanca = new Productos("Chocolatina Hersheys Blanca",
     "Cookies 'n' creme es una barrita de chocolate blanco con bits de galleta similares en sabor y textura a las Oreo.",
     `<img src="img/hersheyblanca.png" class="card-img-top" alt="Hershey's Blanca">`);
-let hersheyNegra = new Productos("Chocolatina Hershey's Negra",
+let hersheyNegra = new Productos("Chocolatina Hersheys Negra",
     "La barrita de chocolate con leche Hershey's, tiene un sabor único característico, amargo y picante. Esta es su variedad chocolate negro.",
     `<img src="img/hersheynegra.png" class="card-img-top" alt="Hershey's Negra" style="padding-top: 7px;">`);
 let chocolatesMYM = new Productos("Chocolates M&M",
@@ -40,13 +40,14 @@ function agregarAlCarrito(tipo) {
                                 <h5 class="card-title">${baseDeDatos[i].nombre}</h5>
                                 <p class="card-text">${baseDeDatos[i].descripcion}</p>
                                 <button type="button" class="btn btn-secondary btn-lg btn-block"
-                                onclick=${"agregarAlBaul(baseDeDatos[0].nombre)"}>Agregar al Baúl</button>
+                                onclick='agregarAlBaul(${JSON.stringify(baseDeDatos[i].nombre)})'>Agregar al Baúl</button>
                             </div>
                         </div>
                     </div>`
     }
 
-    document.getElementById("productos").innerHTML = mostrarProducto;
+    // document.getElementById("productos").innerHTML = mostrarProducto;
+    $("#productos").html(mostrarProducto);
 }
 
 function verCarrito() {
@@ -58,14 +59,14 @@ function verCarrito() {
         for (i = 0; i < carrito.length; i++) {
             mostrarCarrito += `
             <h5>${carrito[i]}
-            <a href="" onclick=${"borrarDelBaul()"}><img
+            <a href="" onclick='borrarDelBaul(${JSON.stringify(carrito[i])})'}><img
                 style="width:25px; float:right;" src="img/eliminar.png" alt="Carrito"></a>
             </h5>
             <hr>
             `
         }
-
-        document.getElementById("carritoCompras").innerHTML = mostrarCarrito;
+        // document.getElementById("carritoCompras").innerHTML = mostrarCarrito;
+        $("#carritoCompras").html(mostrarCarrito);
 
     } else if (localStorage.getItem('carrito') === null) {
         document.getElementById("carritoCompras").innerHTML = `
@@ -79,13 +80,14 @@ function agregarAlBaul(nombre) {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
-function borrarDelBaul() {
+function borrarDelBaul(producto) {
     const nuevoCarrito = [];
+    let produ = producto;
     for (let i = 0; i < carrito.length; i++) {
-        if(i != 1){
+        if (produ !== carrito[i]) {
             nuevoCarrito.push(carrito[i]);
         }
     }
     localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
     carrito = nuevoCarrito;
-  }
+}
